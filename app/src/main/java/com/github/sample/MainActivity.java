4c9fdeb2bdc1595374150079f4b12a2fbfaddc95
gongw <gw_ling@sina.com>
2018-10-20 16:37:11 +0800

    add some sample code

--- app/src/main/java/com/github/sample/MainActivity.java
+++ app/src/main/java/com/github/sample/MainActivity.java
@@ -2,6 +2,8 @@ package com.github.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import com.github.gongw.VerifyCodeView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        VerifyCodeView underLine = findViewById(R.id.vc_underLine);
+        VerifyCodeView centerLine = findViewById(R.id.vc_centerLine);
+        VerifyCodeView square = findViewById(R.id.vc_square);
+        VerifyCodeView circle = findViewById(R.id.vc_circle);
+        underLine.setOnAllFilledListener(listener);
+        centerLine.setOnAllFilledListener(listener);
+        square.setOnAllFilledListener(listener);
+        circle.setOnAllFilledListener(listener);
+
     }
 
-
+    private VerifyCodeView.OnAllFilledListener listener = new VerifyCodeView.OnAllFilledListener() {
+        @Override
+        public void onAllFilled(String text) {
+            Toast.makeText(MainActivity.this, "filled!", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
